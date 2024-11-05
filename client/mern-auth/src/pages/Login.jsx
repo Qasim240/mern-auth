daa1d7df30ade692092a807e7a027303f55dd6b7
unknown <Qasim@Nutri-106.nutrifactor.local>
2024-11-05 16:24:33 +0500

    implement validation using yup and react-hook-form and improve UI/UX

--- client/mern-auth/src/pages/Login.jsx
+++ client/mern-auth/src/pages/Login.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useLoginMutation } from '../redux/baseApi';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../redux/slices/userSlice';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// Validation schema
+const schema = yup.object().shape({
+    email: yup.string().email().required('Email is required'),
+    password: yup.string().min(8, 'Password must be at least 8 characters').required('Password is required')
+});
 
 const Login = () => {
     const [login, { data, error, isLoading }] = useLoginMutation();
-    const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
+    const [serverError, setServerError] = useState('');
+
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+    const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
-
-
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
 
     useEffect(() => {
         if (isLoggedIn) {
@@ -30,16 +28,12 @@ const Login = () => {
         }
     }, [isLoggedIn, navigate]);
 
+    const onSubmit = async (data) => {
+        setServerError(''); // Reset error state before the request
 
-
-
-    const onSubmit = async (e) => {
-        e.preventDefault();
         try {
-            const response = await login(formData).unwrap();
-            // console.log("User logged in successfully", response);
-            const userData = response.data
-            // Dispatch the user data to Redux store
+            const response = await login(data).unwrap();
+            const userData = response.data;
             dispatch(setUser({
                 message: userData.message,
                 id: userData._id,
@@ -47,36 +41,81 @@ const Login = () => {
                 email: userData.email,
                 token: userData.token,
             }));
-
             navigate("/dashboard");
-        } catch (error) {
-            console.log("Error", error);
+        } catch (err) {
+
+            if (err.data) {
+                setServerError(err.data);
+            } else {
+                console.error("Error", err);
+            }
         }
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                style={{ margin: '10px' }}
-                type="email"
-                placeholder='Email'
-                name='email'
-                value={formData.email}
-                onChange={handleInputChange}
-            />
-            <input
-                type="password"
-                placeholder='Password'
-                name='password'
-                value={formData.password}
-                onChange={handleInputChange}
-            />
-            <div>
-                <button>  <Link to="/">signup</Link></button>
-                <button type='submit'>Login</button>
+        <section className="bg-gray-900 min-h-screen flex items-center justify-center relative">
+            {isLoading && (
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div className="w-16 h-16 border-4 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
+                </div>
+            )}
 
+            <div className={`flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ${isLoading ? "opacity-50" : ""}`}>
+                <div className="w-full max-w-md bg-gray-800 rounded-lg shadow dark:border dark:border-gray-700">
+                    <div className="p-6 space-y-6">
+                        <h1 className="text-xl font-bold leading-tight text-white md:text-2xl">
+                            Sign in to your account
+                        </h1>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                <input
+                                    {...register('email')}
+                                    type="email"
+                                    id="email"
+                                    className={`bg-gray-700 border ${errors.email ? "border-red-500 focus:ring-red-500" : "border-gray-600"} text-white rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                                    placeholder="name@company.com"
+                                />
+                                {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+                                <input
+                                    {...register('password')}
+                                    type="password"
+                                    id="password"
+                                    className={`bg-gray-700 border ${errors.password ? "border-red-500 focus:ring-red-500" : "border-gray-600"} text-white rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                                    placeholder="••••••••"
+                                />
+                                {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <input
+                                        id="remember"
+                                        type="checkbox"
+                                        className="w-4 h-4 border border-gray-600 rounded bg-gray-700 focus:ring-3 focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    />
+                                    <label htmlFor="remember" className="ml-3 text-sm text-gray-300">Remember me</label>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-white bg-gray-700 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                disabled={isLoading}
+                            >
+                                {isLoading ? 'Signing in...' : 'Sign in'}
+                            </button>
+                            {serverError && <p className="text-red-500 text-sm">{serverError}</p>} 
+
+                            <p className="text-sm font-light text-gray-400">
+                                Don’t have an account yet? <Link to="/" className="font-medium text-primary-500 hover:underline">Sign up</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </form>
+        </section>
     );
 };
 
--- client/mern-auth/src/pages/SignUp.jsx
+++ client/mern-auth/src/pages/SignUp.jsx
@@ -1,73 +1,93 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSignUpMutation } from '../redux/baseApi';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// Validation schema
+const schema = yup.object().shape({
+    name: yup.string().required('Name is required'),
+    email: yup.string().email('Invalid email').required('Email is required'),
+    password: yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
+});
 
 const SignUp = () => {
     const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-    });
+    const [signUp, { error, isLoading }] = useSignUpMutation();
 
-    // Use the signUp mutation hook
-    const [signUp, { data, error, isLoading }] = useSignUpMutation();
+    // Set up react-hook-form
+    const { register, handleSubmit, formState: { errors }, } = useForm({ resolver: yupResolver(schema), });
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const onSubmit = async (e) => {
-        e.preventDefault();
+    // Form submission
+    const onSubmit = async (data) => {
         try {
-            // Call the signUp mutation
-            const response = await signUp(formData).unwrap();
-
-            // User registration was successful, navigate to login
+            const response = await signUp(data).unwrap();
             console.log("User registered successfully", response);
-
             navigate("/login");
-
         } catch (error) {
             console.log("Could not signup", error);
         }
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                type="text"
-                placeholder='Name'
-                name='name'
-                value={formData.name}
-                onChange={handleInputChange}
-            />
-            <input
-                style={{ margin: '10px' }}
-                type="email"
-                placeholder='Email'
-                name='email'
-                value={formData.email}
-                onChange={handleInputChange}
-            />
-            <input
-                type="password"
-                placeholder='Password'
-                name='password'
-                value={formData.password}
-                onChange={handleInputChange}
-            />
-            <div>
-                <button><Link to="/login">Login</Link></button>
-                <button type='submit' disabled={isLoading}>Signup</button>
-                {isLoading && <p>Signing up...</p>}
-                {error && <p style={{ color: 'red' }}>Error: {error.message}</p>}
+        <section className="bg-gray-900 min-h-screen flex items-center justify-center">
+              {isLoading && (
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div className="w-16 h-16 border-4 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
+                </div>
+            )}
+            <div className="w-full max-w-md bg-gray-800 rounded-lg shadow dark:border dark:border-gray-700">
+                <div className="p-6 space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold text-white">
+                        Create an account
+                    </h1>
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-white">Your Name</label>
+                            <input
+                                {...register('name')}
+                                className={`bg-gray-700 border ${errors.name ? "border-red-500 focus:ring-red-500 border-2" : "border-gray-600"} text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-white`}
+                                type="text"
+                                placeholder="Name"
+                            />
+                            {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-white">Your Email</label>
+                            <input
+                                {...register('email')}
+                                className={`bg-gray-700 border ${errors.email ? "border-red-500 focus:ring-red-500 border-2" : "border-gray-600"} text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-white`}
+                                type="email"
+                                placeholder="name@company.com"
+                            />
+                            {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-white">Password</label>
+                            <input
+                                {...register('password')}
+                                className={`bg-gray-700 border ${errors.password ? "border-red-500 focus:ring-red-500 border-2" : "border-gray-600"} text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-white`}
+                                type="password"
+                                placeholder="••••••••"
+                            />
+                            {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full text-white bg-gray-700 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                            disabled={isLoading}
+                        >
+                            {isLoading ? 'Signing up...' : 'Sign up'}
+                        </button>
+                        <p className="text-sm font-light text-gray-400">
+                            Already have an account? <Link to="/login" className="font-medium text-white hover:underline">Login here</Link>
+                        </p>
+                        {error && <p className="text-red-500 text-sm">Error: {error.message}</p>}
+                    </form>
+                </div>
             </div>
-        </form>
+        </section>
     );
 };
 
